8e72f25c22cc066cf2e4539323c03388e2c933d9
Bill Page <billp778@gmail.com>
2018-12-10 01:21:39 -0600

    Re sized city level added money to collect to go to next level

---
+++ Assets/Scripts/ToLevel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToLevel : MonoBehaviour {
+
+    public string level;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(level);
+        }
+    }
+}
